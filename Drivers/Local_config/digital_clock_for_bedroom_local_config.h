7d1aef52563b22611632158f278e3b250d1e0744
Бажен Левковець <bazhen.paseka@gmail.com>
2021-04-07 23:04:33 +0300

    6 buttons work. Step 2.

--- Drivers/Local_config/digital_clock_for_bedroom_local_config.h
+++ Drivers/Local_config/digital_clock_for_bedroom_local_config.h
@@ -8,5 +8,8 @@
 	#define VERSION_MONTH	4
 	#define VERSION_DAY		7
 
+	#define	BUTTON_DELAY	100
+	#define	BEEPER_DELAY	 50
+
 //******************************************************************************************
 #endif	//	DIGITAL_CLOCK_FOR_BEDROOM_CONFIG_INCLUDED_H
